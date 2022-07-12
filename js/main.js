c1e04bf9ea818b93e5fe763ef91f94a4e2265341
Laziz-Abdurasulov <lenova5192022@gmail.com>
2022-07-12 22:29:33 +0500

    add new ul

--- js/main.js
+++ js/main.js
@@ -4,14 +4,20 @@ let elList = document.querySelector(".js-list");
 let elAll = document.querySelector(".js-all");
 let elCompleted = document.querySelector(".js-completed");
 let elUnCompleted = document.querySelector(".js-uncompleted");
+let elBtnGroup = document.querySelector(".right");
+let elUl = document.querySelector(".new-ul");
 
-let todos = [];
+const localTodos = JSON.parse(window.localStorage.getItem("list"));
+let todos = localTodos || [];
+let bookmarkTodos = []
 
 
 
 let renderTodo = (array, node) => {
-    let result1 = 0; 
-    let result2 = 0;
+    elAll.textContent = todos.length;
+    elCompleted.textContent = todos.filter(e => e.isComplete).length;
+    elUnCompleted.textContent = todos.filter(e => !e.isComplete).length;
+    node.innerHTML = "";
     
     array.forEach(element => {
         let newItem = document.createElement("li");
@@ -29,66 +35,68 @@ let renderTodo = (array, node) => {
         newButton.style.color = "white";
         newButton.style.border = "none";
         newButton.style.padding = "5px";
+        let Bookmark = document.createElement("button");
         let newCheckbox = document.createElement("input");
         
         newSpan.textContent = element.name;
         newButton.textContent = "Delete"
+        Bookmark.textContent = "Bookmark"
         newButton.setAttribute("class", "delete-btn");
         newButton.dataset.todoId = element.id;
+        Bookmark.dataset.todoId = element.id;
         newCheckbox.setAttribute("type", "checkbox");
         newCheckbox.setAttribute("class", "todo-check");
         newCheckbox.dataset.todoId = element.id;
+        Bookmark.addEventListener("click", function(){
+            
+            let newItem2 = document.createElement("li");
+            newItem2.textContent = element.name;
+            elUl.appendChild(newItem2);
+            
+        })
         
         if (element.isComplete) {
             newSpan.style.textDecoration = "line-through"
             newCheckbox.checked = true;
-            result1+= 1;
-        }else{
-            result2+= 1;
+            
         }
         
         newItem.appendChild(newCheckbox);
         newItem.appendChild(newSpan);
         newItem.appendChild(newButton);
+        newItem.appendChild(Bookmark)
         node.appendChild(newItem);
     });
-    
-    elCompleted.textContent = result1;
-    elUnCompleted.textContent = result2;
-    
 };
 
-
+renderTodo(todos, elList);
 
 elList.addEventListener("click", function (evt) {
     if (evt.target.matches(".delete-btn")) {
         let deletedId = evt.target.dataset.todoId;
-        elList.innerHTML = "";
         let findedIndex = todos.findIndex((todo) => todo.id == deletedId);
         
-    todos.splice(findedIndex, 1);
-        elUnCompleted.textContent = todos.length;
-        elAll.textContent = todos.length;
+        todos.splice(findedIndex, 1);
+        
         renderTodo(todos, elList);
+        window.localStorage.setItem("list", JSON.stringify(todos));
     } else if (evt.target.matches(".todo-check")) {
         let checkedId = evt.target.dataset.todoId;
-        elList.innerHTML = "";
         let findedElement = todos.find((todo) => todo.id == checkedId)
         
         findedElement.isComplete = !findedElement.isComplete;
         renderTodo(todos, elList);
+        window.localStorage.setItem("list", JSON.stringify(todos));
     }
 });
-console.log(elUnCompleted.textContent);
-console.log(elAll.textContent);
+
 
 
 elForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
     elList.innerHTML = "";
     let elInputVal = elInput.value;
-    elAll.textContent = todos.length + 1;
-    elUnCompleted.textContent = todos.length + 1;
+    
     
     let obj = {
         id: todos.length ? todos[todos.length - 1].id + 1 : 0,
@@ -97,6 +105,28 @@ elForm.addEventListener("submit", function (evt) {
     }
     todos.push(obj)
     renderTodo(todos, elList);
+    window.localStorage.setItem("list", JSON.stringify(todos));
     elInput.value = "";
-    console.log(obj);
 });
+
+elBtnGroup.addEventListener("click", function(evt){
+    if(evt.target.matches(".js-buttona")){
+        renderTodo(todos, elList);
+    }
+    if(evt.target.matches(".js-buttonc")){
+        const completedTodos = todos.filter(e => e.isComplete);
+        renderTodo(completedTodos, elList);
+    }
+    if(evt.target.matches(".js-buttonu")){
+        const unCompletedTodos = todos.filter(e => !e.isComplete);
+        renderTodo(unCompletedTodos, elList);
+    }
+    if (evt.target.matches(".clear-button")) {
+        window.localStorage.removeItem("list");
+        window.location.reload();
+        renderTodo(todos, elList);
+    }
+});
+
+
+
